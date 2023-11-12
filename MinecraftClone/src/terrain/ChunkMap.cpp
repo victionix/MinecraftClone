9cdc7c25e3176837a169388b31620e67725d91c8
Victor Hubert <121970968+victionix@users.noreply.github.com>
2023-11-12 14:39:11 +0100

    Update ChunkMap.cpp

--- MinecraftClone/src/terrain/ChunkMap.cpp
+++ MinecraftClone/src/terrain/ChunkMap.cpp
@@ -45,9 +45,6 @@ void terrain::ChunkMap::addChunk(const glm::ivec3 position)
 	auto chunk = std::make_shared<terrain::ChunkMesh>(position);
 	m_Chunks.emplace(position,std::move(chunk));
 	m_WorldGenerator.GenerateChunk(m_Chunks[position]);
-
-	m_WorldGenerator.GenerateChunk(chunk);
-	m_Chunks.emplace(position, std::move(chunk));
 	for (uint16_t i = 0; i < 26; i++)
 	{
 		glm::ivec3 offset = position + terrain::Chunk::m_NeighboursOffset[i];
@@ -67,25 +64,6 @@ void terrain::ChunkMap::BuildChunks()
 		chunk.second->Build();
 	}
 }
-
-
-void terrain::ChunkMap::BuildNeighboursChunks(const glm::ivec3 position)
-{
-	for (uint32_t i = 0; i < 26; i++)
-	{
-		glm::ivec3 offset = position + terrain::Chunk::m_NeighboursOffset[i];
-		std::weak_ptr<Chunk> accessNeighbour = find(offset);
-		if (auto neigh = accessNeighbour.lock())
-		{
-			//if (neigh->m_ShouldRebuild)
-			//{
-			//		
-			//		neigh->Build();
-			//}
-		}
-	}
-}
-
 void terrain::ChunkMap::updateChunks(const glm::ivec2 oldCenterPosition, const glm::ivec2 newCenterPosition)
 {
 	util::Log<glm::ivec2>(newCenterPosition, "Current chunk Position");
